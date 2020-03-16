1e792caa7c6bcf554aaeb7f586917677a3ebc011
Cedric Finance <cedric.finance@finandric.com>
2020-03-16 08:58:49 +0100

    Refactoring

--- application/slack.go
+++ application/slack.go
@@ -3,8 +3,10 @@ package application
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/CedricFinance/slackpaul/domain/entities"
 	"github.com/nlopes/slack"
 	"net/http"
+	"strconv"
 )
 
 func WriteError(w http.ResponseWriter, err error) {
@@ -34,4 +36,61 @@ func WriteJSON(w http.ResponseWriter, d interface{}) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(res)
+}
+
+func ConfigurePoll(args []string, channelId string, ownerId string) (entities.Poll, error) {
+	maxVotes := 1
+	maxVotesPerProposition := 0
+	anonymous := false
+
+	optionFound := true
+	for optionFound && len(args) > 0 {
+		optionFound = false
+
+		if args[0] == "limit" && len(args) >= 5 {
+			var err error
+			maxVotes, err = strconv.Atoi(args[1])
+			if err != nil {
+				return entities.Poll{}, fmt.Errorf("%q is not a valid value for the max number of vote per participant", args[1])
+			}
+			args = args[2:]
+			optionFound = true
+		}
+
+		if args[0] == "max" && len(args) >= 5 {
+			var err error
+			maxVotesPerProposition, err = strconv.Atoi(args[1])
+			if err != nil {
+				return entities.Poll{}, fmt.Errorf("%q is not a valid value for the max number of vote per choice", args[1])
+			}
+			args = args[2:]
+			optionFound = true
+		}
+
+		if args[0] == "anonymous" && len(args) >= 4 {
+			anonymous = true
+
+			args = args[1:]
+			optionFound = true
+		}
+
+	}
+
+	if len(args) < 3 {
+		return entities.Poll{}, fmt.Errorf("a poll must have at least a title and two choices")
+	}
+
+	title := args[0]
+	propositions := args[1:]
+
+	if maxVotes < 1 {
+		maxVotes = len(propositions) + maxVotes
+	}
+
+	poll := entities.NewPoll(title, propositions, channelId, ownerId)
+	poll.MaxVotes = maxVotes
+	poll.Anonymous = anonymous
+	poll.MaxVotesPerProposition = maxVotesPerProposition
+
+	return poll, nil
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/CedricFinance/slackpaul
 go 1.11
 
 require (
+	github.com/go-playground/form v3.1.4+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/google/uuid v1.1.1
 	github.com/mattn/go-shellwords v1.0.5
 	github.com/nlopes/slack v0.5.1-0.20190605211732-a05dfd3f167d
 	github.com/spf13/viper v1.4.0
+	github.com/stretchr/testify v1.2.2
 )
